3211f9e6a57794de3e00c35231802976a75b38c9
rohithsuryaneni-rao <suryanenimahesh@gmail.com>
2024-10-23 10:20:30 +0530

    Add files via upload

---
+++ dockerspringboot/src/main/java/com/example/docker/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.docker.controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/")
+public class HelloController {
+	//@GetMapping("/hello")
+	public String helloo()
+	{
+		return "helloo hiii byeee";
+	}
+}
